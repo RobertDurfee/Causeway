ad3258b85a9a46fd9a6c19bf4cdcd4e12c14913f
Robert Durfee <robert.durfee.17@gmail.com>
2020-09-26 14:56:36 -0500

    secured

--- cli/main.js
+++ cli/main.js
@@ -1,61 +1,89 @@
+const https = require('https')
+const fs = require('fs')
 const WebSocket = require('ws')
 const program = require('commander')
 
 const server = () => {
-    const wss = new WebSocket.Server({ port: 0 })
+    const server = https.createServer({
+        cert: fs.readFileSync('my.cert.pem'),
+        ca: [ fs.readFileSync('my.cert.pem') ],
+        key: fs.readFileSync('my.key.pem'),
+        passphrase: fs.readFileSync('my.key.pem.pass', { encoding: 'utf-8' }),
+        requestCert: true,
+        rejectUnauthorized: true
+    })
+    const wss = new WebSocket.Server({
+        verifyClient: info => {
+            return !!info.req.client.authorized
+        },
+        server
+    })
     wss.on('listening', () => {
-        console.error(`[cw] Local ws://localhost:${wss.address().port}/ listening`)
+        console.error(`[cw] Local wss://localhost:${wss.address().port}/ listening`)
         console.error(`[cw] Waiting for connection...`)
     })
     wss.on('connection', (ws, req) => {
-        console.error(`[cw] Remote ws://${req.socket.remoteAddress}:${req.socket.remotePort}/ connected`)
+        console.error(`[cw] Remote wss://${req.socket.remoteAddress}:${req.socket.remotePort}/ connected`)
         const duplex = WebSocket.createWebSocketStream(ws, { encoding: 'utf-8' })
         duplex.on('end', () => {
-            console.error(`[cw] Remote ws://${req.socket.remoteAddress}:${req.socket.remotePort}/ ended`)
+            console.error(`[cw] Remote wss://${req.socket.remoteAddress}:${req.socket.remotePort}/ ended`)
             duplex.unpipe(process.stdout)
             process.stdin.unpipe(duplex)
         });
         duplex.on('error', () => {
-            console.error(`[cw] Remote ws://${req.socket.remoteAddress}:${req.socket.remotePort}/ errored`)
+            console.error(`[cw] Remote wss://${req.socket.remoteAddress}:${req.socket.remotePort}/ errored`)
             duplex.unpipe(process.stdout)
             process.stdin.unpipe(duplex)
         });
         process.stdin.on('end', () => {
-            console.error(`[cw] Local ws://localhost:${wss.address().port}/ ended`)
+            console.error(`[cw] Local wss://localhost:${wss.address().port}/ ended`)
             process.stdin.unpipe(duplex)
         });
         process.stdin.on('error', () => {
-            console.error(`[cw] Local ws://localhost:${wss.address().port}/ errored`)
+            console.error(`[cw] Local wss://localhost:${wss.address().port}/ errored`)
             process.stdin.unpipe(duplex)
         });
         duplex.pipe(process.stdout)
         process.stdin.pipe(duplex)
     })
+    server.listen(0)
 }
 
 const client = url => {
     console.error(`[cw] Waiting for connection...`)
-    const ws = new WebSocket(url)
+    const ws = new WebSocket(`wss://${url}/`, {
+        cert: fs.readFileSync('my.cert.pem'),
+        ca: [ fs.readFileSync('my.cert.pem') ],
+        key: fs.readFileSync('my.key.pem'),
+        passphrase: fs.readFileSync('my.key.pem.pass', { encoding: 'utf-8' }),
+        rejectUnathorized: true,
+        checkServerIdentity: (hostname, cert) => {
+            const cn = fs.readFileSync('my.cert.pem.cn')
+            if (cert.subject.CN != cn) {
+                return new Error(`Remote CN '${cert.subject.CN}' does not match '${cn}'`)
+            }
+        },
+    })
     ws.on('open', () => {
-        console.error(`[cw] Remote ${url} opened`)
+        console.error(`[cw] Remote wss://${url}/ opened`)
     })
     const duplex = WebSocket.createWebSocketStream(ws, { encoding: 'utf-8' })
     duplex.on('end', () => {
-        console.error(`[cw] Remote ${url} ended`)
+        console.error(`[cw] Remote wss://${url}/ ended`)
         duplex.unpipe(process.stdout)
         process.stdin.unpipe(duplex)
     })
-    duplex.on('error', () => {
-        console.error(`[cw] Remote ${url} errored`)
+    duplex.on('error', error => {
+        console.error(`[cw] Remote wss://${url}/ errored: ${error.message}`)
         duplex.unpipe(process.stdout)
         process.stdin.unpipe(duplex)
     })
     process.stdin.on('end', () => {
-        console.error(`[cw] Local ws://localhost:????/ ended`)
+        console.error(`[cw] Local wss://localhost:????/ ended`)
         process.stdin.unpipe(duplex)
     });
     process.stdin.on('error', () => {
-        console.error(`[cw] Local ws://localhost:????/ errored`)
+        console.error(`[cw] Local wss://localhost:????/ errored`)
         process.stdin.unpipe(duplex)
     });
     duplex.pipe(process.stdout)
@@ -64,7 +92,7 @@ const client = url => {
 
 program
     .version('1.0.0')
-    .description('Command-line interface for a remote, bidirectional pipe over secure WebSockets with certificate authentication.')
+    .description('Command-line interface for an encrypted, authenticated bidirectional pipe over WebSockets.')
     .option('-d, --destination <destination>', 'Destination WebSocket server.')
     .action(options => {
         if (options.destination) {
